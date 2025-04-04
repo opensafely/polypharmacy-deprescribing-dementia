7a208969545d10d9450e30d9a7af5196a3d0d144
Rob Porteous <zh24495@bristol.ac.uk>
2025-04-04 14:41:27 +0000

    Adding dementia codelists and other variables needed to define the study population (as per the protocol), eg age, practice registration, alive, sex, imd, rgion

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,17 +1,48 @@
-from ehrql import create_dataset
-from ehrql.tables.tpp import patients, practice_registrations
+from ehrql import create_dataset, codelist_from_csv
+from ehrql.tables.tpp import patients, practice_registrations, clinical_events, addresses
 
+##Create dataset
 dataset = create_dataset()
 
-index_date = "2020-03-31"
+##Set start date
+index_date = "2015-01-01"
+end_date = "2015-12-31"
+##Create codelist objects
+dementia_codelist = codelist_from_csv(
+    #Need to decide which codelist to use "codelists/bristol-any-dementia-snomed-ct-v14.csv", 
+    "codelists/nhsd-primary-care-domain-refsets-dem_cod.csv",
+    column="code"
+)
 
+##Derive dataset variables
+dataset.sex = patients.sex
+dataset.age = patients.age_on(index_date)
+dataset.latest_dementia_code = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(dementia_codelist))
+    .where(clinical_events.date.is_on_or_before(end_date))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    .snomedct_code)
+dataset.imd = addresses.for_patient_on(index_date).imd_rounded
+dataset.region = practice_registrations.for_patient_on(index_date).practice_nuts1_region_name
+
+##Apply study population criteria from protocol
+aged_65_or_above = dataset.age > 64
 has_registration = practice_registrations.for_patient_on(
     index_date
 ).exists_for_patient()
 
+has_dementia = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(dementia_codelist))
+    .where(clinical_events.date.is_on_or_before(end_date))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    ).exists_for_patient()
 
-dataset.sex = patients.sex
-dataset.age = patients.age_on(index_date)
+is_alive = patients.is_alive_on(index_date)
+known_sex = patients.sex != "unknown"
+known_imd = (dataset.imd >= 0)
+known_region = dataset.region != ""
 
-aged_above_65 = dataset.age>65
-dataset.define_population(has_registration & aged_above_65)
+##Define population
+dataset.define_population(has_registration & has_dementia & aged_65_or_above & is_alive & known_sex & known_imd & known_region)
