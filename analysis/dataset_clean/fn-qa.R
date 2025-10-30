f8ec375974f3b6813368cecea0197b7daab2aa55
Rob Porteous <zh24495@bristol.ac.uk>
2025-10-30 16:00:37 +0000

    Death date logic
    Fixed issue with death date QA excluding everyone

--- analysis/dataset_clean/fn-qa.R
+++ analysis/dataset_clean/fn-qa.R
@@ -14,35 +14,29 @@ qa <- function(input, flow) {
       (input$qa_num_birth_year < format(Sys.Date(),"%Y")),
   ]
   flow[nrow(flow) + 1, ] <- c(
-    "Quality assurance: Year of birth is before today",
+    "Quality assurance: Year of birth must be in the past",
     nrow(input)
   )
   print(flow[nrow(flow), ])
 
   # Date of death not in the future
   input <- input[
-    !is.na(input$qa_num_death_year) &
+    is.na(input$qa_num_death_year) |
       (input$qa_num_death_year < format(Sys.Date(),"%Y")),
   ]
   flow[nrow(flow) + 1, ] <- c(
-    "Quality assurance: Year of death is before today",
+    "Quality assurance: Year of death must be in the past",
     nrow(input)
   )
   print(flow[nrow(flow), ])
 
   # Missing sex
   input <- subset(input, cov_cat_sex == "female" | cov_cat_sex == "male" | cov_cat_sex == "intersex")
-    flow[nrow(flow) + 1, ] <- c(
-        "Quality assurance: Known sex",
-        nrow(input)
-    )
+  flow[nrow(flow) + 1, ] <- c("Quality assurance: Known sex", nrow(input))
 
   # Missing IMD
-    input <- subset(input, cov_cat_imd %in% c("1 (most deprived)", "2", "3", "4", "5 (least deprived)"))
-        flow[nrow(flow) + 1, ] <- c(
-            "Quality assurance: Known IMD",
-            nrow(input)
-        )
-
+  input <- subset(input, cov_cat_imd %in% c("1 (most deprived)", "2", "3", "4", "5 (least deprived)"))
+  flow[nrow(flow) + 1, ] <- c("Quality assurance: Known IMD", nrow(input))
+  
   return(list(input = input, flow = flow))
 }
