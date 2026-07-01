2698fd93981eb98803ee9d277d8c13c92daa7e8b
Rob Porteous <zh24495@bristol.ac.uk>
2026-07-01 16:00:46 +0100

    reducing limit

--- analysis/dataset_definition/dataset_definition_hist.py
+++ analysis/dataset_definition/dataset_definition_hist.py
@@ -24,7 +24,7 @@ dataset = create_dataset()
 from analysis.dataset_definition.study_dates import *
 
 index_date = start_date
-limit = 52
+limit = 26
 
 # Add inex variables for 2015 through 2024
 for year in range(2017, 2025):
