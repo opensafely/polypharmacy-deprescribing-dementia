066892a3cd25251cf93d65ee1e6047a03f461763
Rob Porteous <zh24495@bristol.ac.uk>
2026-08-13 10:48:01 +0100

    Updating new ethnicity handling

--- analysis/dataset_clean/fn-ref.R
+++ analysis/dataset_clean/fn-ref.R
@@ -29,11 +29,36 @@ ref <- function(input, suffix = "", describe = TRUE) {
   # Handle missing values in cov_cat_ethnicity ---------------------------------
 
   if ("cov_cat_ethnicity" %in% names(input)) {
-    print("Handle missing values in cov_cat_ethnicity")
+    print('Handle missing values in cov_cat_ethnicity')
     input$cov_cat_ethnicity <- if_else(
-      input$cov_cat_ethnicity %in% c("1", "2", "3", "4", "5"),
+      input$cov_cat_ethnicity %in%
+        c(
+          # 6 category labels
+          "White",
+          "Mixed",
+          "Asian",
+          "Black",
+          "Other",
+          # 16 category labels
+          "Other White",
+          "White and African",
+          "All other ethnic groups",
+          "African",
+          "White Irish",
+          "Other Mixed",
+          "White British",
+          "Caribbean",
+          "Other Black",
+          "White and Caribbean",
+          "Other Asian",
+          "Chinese",
+          "Pakistani",
+          "White and Asian",
+          "Indian",
+          "Bangladeshi"
+        ),
       input$cov_cat_ethnicity,
-      "0"
+      "Missing"
     )
   }
 
@@ -89,16 +114,15 @@ ref <- function(input, suffix = "", describe = TRUE) {
   # Set reference level for variable: cov_cat_ethnicity --------------------
 
   if ("cov_cat_ethnicity" %in% names(input)) {
-    print("Set reference level for variable: cov_cat_ethnicity")
-    levels(input$cov_cat_ethnicity) <- list(
-      "Missing" = "0",
-      "White" = "1",
-      "Mixed" = "2",
-      "Asian" = "3",
-      "Black" = "4",
-      "Other" = "5"
-    )
-    input$cov_cat_ethnicity <- relevel(input$cov_cat_ethnicity, ref = "White")
+    print('Set reference level for variable: cov_cat_ethnicity')
+    if ("White British" %in% unique(input$cov_cat_ethnicity)) {
+      input$cov_cat_ethnicity <- relevel(
+        input$cov_cat_ethnicity,
+        ref = "White British"
+      )
+    } else {
+      input$cov_cat_ethnicity <- relevel(input$cov_cat_ethnicity, ref = "White")
+    }
   }
 
   # Set reference level for variable: cov_cat_region ---------------------------
